59b024fa2849fff1f6115b8e51b66763a55a94bb
Jordan Forbes <jordforb@gmail.com>
2023-01-16 15:28:17 -0500

    useDamage hook returns object

--- src/App.js
+++ src/App.js
@@ -3,11 +3,12 @@ import "./App.css";
 import { DropdownButton } from "react-bootstrap";
 import SpellDetails from "./components/SpellDetails.jsx";
 import SkillButton from "./components/SkillButton.jsx";
+import useDamage from "./hooks/useDamage";
 
 function App() {
   const [allSpells, setAllSpells] = useState([]);
   const [skillObj, setSkillObj] = useState();
-  const [hasDmg, setHasDmg] = useState(false);
+  const dmgObj = useDamage(skillObj);
 
   useEffect(() => {
     console.log("effect triggered");
@@ -21,34 +22,10 @@ function App() {
   useEffect(() => {
     console.log("og skill object", skillObj);
     if (skillObj) {
-      spellSlots();
-    }
-  }, [skillObj]);
-
-  const spellSlots = () => {
-    let dmgObj;
-    if (skillObj["damage"]) {
-      dmgObj = skillObj["damage"];
-      console.log("does damage");
       console.log(dmgObj);
-      setHasDmg(true);
-      if (dmgObj["damage_at_character_level"]) {
-        console.log("level modifier");
-        console.log(dmgObj["damage_at_character_level"]);
-      } else {
-        console.log("not controlled by level");
-      }
-      if (dmgObj["damage_at_slot_level"]) {
-        console.log("slot modifier");
-        console.log(dmgObj["damage_at_slot_level"]);
-      } else {
-        console.log("not controlled by slot");
-      }
-    } else {
-      console.log("no damage");
-      setHasDmg(false);
     }
-  };
+    console.log("damage object", dmgObj);
+  }, [skillObj]);
 
   return (
     <div className="App">
--- src/hooks/useDamage.jsx
+++ src/hooks/useDamage.jsx
@@ -1,10 +1,43 @@
 import { useState, useEffect } from "react";
 
 const useDamage = (skill) => {
-  const [hasDamage, setHasDamage] = useEffect(false);
-  const [hasHeal, setHasHeal] = useEffect(false);
-  const [checksSpellSlots, setChecksSpellSlots] = useEffect(false);
-  const [checksLevel, setChecksLevel] = useEffect(false);
+  const [name, setName] = useState("null");
+  const [hasDamage, setHasDamage] = useState(false);
+  const [hasHeal, setHasHeal] = useState(false);
+  const [checksSpellSlots, setChecksSpellSlots] = useState(false);
+  const [checksLevel, setChecksLevel] = useState(false);
+  const [damageType, setDamageType] = useState("null");
+
+  useEffect(() => {
+    if (skill) {
+      setName(skill["name"]);
+      if (skill["damage"]) {
+        setHasDamage(true);
+        if (skill["damage"]["damage_at_character_level"]) {
+          setChecksLevel(true);
+        }
+        if (skill["damage"]["damage_at_slot_level"]) {
+          setChecksSpellSlots(true);
+        }
+        if (skill["damage"]["damage_type"]) {
+          setDamageType(skill["damage"]["damage_type"]["name"]);
+        }
+      }
+      if (skill["heal_at_slot_level"]) {
+        setHasHeal(true);
+        setChecksSpellSlots(true);
+      }
+    }
+  }, [skill]);
+
+  return {
+    name: name,
+    hasDamage: hasDamage,
+    hasHeal: hasHeal,
+    checksLevel: checksLevel,
+    checksSpellSlots: checksSpellSlots,
+    damageType: damageType,
+  };
 };
 
 export default useDamage;
