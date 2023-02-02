434100d0d3da4a1c65cc1d4d12abdf6fd6a94be7
Jordan Forbes <jordforb@gmail.com>
2023-02-02 11:12:55 -0500

    rolls now work

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -23,12 +23,14 @@ import {
   resetSkill,
 } from "../../features/skillSelector/skillSelectorSlice";
 // const areScales = useSelector((state) => state.groupSelector.areScales);
+import droll from "droll";
 
 function App() {
   const [allSpells, setAllSpells] = useState([]);
   const [skillObj, setSkillObj] = useState();
   const [charLevel, setCharLevel] = useState(1);
   const [spellSlot, setSpellSlot] = useState(1);
+  const [roll, setRoll] = useState("1d20");
 
   const dispatch = useDispatch();
   const thisSkillObj = useSelector((state) => state.skillSelector);
@@ -68,48 +70,68 @@ function App() {
     }
   }, [skillObj]);
 
+  const RollColumn = (props) => {
+    const [rollResult, setRollResult] = useState(-1);
+    useEffect(() => {
+      let temp = droll.roll(props.roll);
+      console.log("props.roll", props.roll);
+      console.log("temp", temp);
+      setRollResult(droll.roll(props.roll));
+      console.log(rollResult);
+    }, [props.roll]);
+    return (
+      <div className="col-md-1 rollColumn">
+        <h4>Roll</h4>
+        <h2>{rollResult.total}</h2>
+      </div>
+    );
+  };
+
   return (
     <div className="App">
       <div className="container">
         <br />
-        <SkillColumn
-          allSpells={allSpells}
-          skillObj={skillObj}
-          setSkillObj={setSkillObj}
-        />
-        <div className="col-md-6 ">
-          <div className="row statsColumn">
-            <StatDetails
-              dmgObj={dmgObj}
-              skillObj={skillObj}
-              setSkillObj={setSkillObj}
-              spellSlot={spellSlot}
-              setSpellSlot={setSpellSlot}
-              charLevel={charLevel}
-              setCharLevel={setCharLevel}
-              allSpells={allSpells}
-              thisSkillObj={thisSkillObj}
-            />
-          </div>
-          <div className="row">
-            <div className=" detailsColumn">
-              {skillObj ? (
-                <SpellDetails
-                  skillObj={skillObj}
-                  setSkillObj={setSkillObj}
-                  spellSlot={spellSlot}
-                  setSpellSlot={setSpellSlot}
-                  dmgObj={dmgObj}
-                  charLevel={charLevel}
-                  setCharLevel={setCharLevel}
-                />
-              ) : (
-                ""
-              )}
+        <div className="row">
+          <SkillColumn
+            allSpells={allSpells}
+            skillObj={skillObj}
+            setSkillObj={setSkillObj}
+          />
+          <div className="col-md-6 ">
+            <div className="row statsColumn">
+              <StatDetails
+                dmgObj={dmgObj}
+                skillObj={skillObj}
+                setSkillObj={setSkillObj}
+                spellSlot={spellSlot}
+                setSpellSlot={setSpellSlot}
+                charLevel={charLevel}
+                setCharLevel={setCharLevel}
+                allSpells={allSpells}
+                thisSkillObj={thisSkillObj}
+              />
+            </div>
+            <div className="row">
+              <div className=" detailsColumn">
+                {skillObj ? (
+                  <SpellDetails
+                    skillObj={skillObj}
+                    setSkillObj={setSkillObj}
+                    spellSlot={spellSlot}
+                    setSpellSlot={setSpellSlot}
+                    dmgObj={dmgObj}
+                    charLevel={charLevel}
+                    setCharLevel={setCharLevel}
+                  />
+                ) : (
+                  ""
+                )}
+              </div>
             </div>
           </div>
+          <SlotColumn setRoll={setRoll} />
+          <RollColumn roll={roll} />
         </div>
-        <SlotColumn />
       </div>
     </div>
   );
--- src/components/SlotColumn/SlotColumn.jsx
+++ src/components/SlotColumn/SlotColumn.jsx
@@ -3,6 +3,8 @@ import { useDispatch, useSelector } from "react-redux";
 import { useEffect, useState } from "react";
 import { Button } from "react-bootstrap";
 const SlotColumn = (props) => {
+  const thisSkillObj = useSelector((state) => state.skillSelector);
+
   const [openSlots, setOpenSlots] = useState({
     8: "8d44",
     20: "2d39",
@@ -13,42 +15,71 @@ const SlotColumn = (props) => {
   // const [valueSlots, setValueSlots] = useState();
   // const skillObj = useSelector((state) => state.skillSelector);
 
-  useEffect(() => {
-    console.log(openSlots);
-    console.log(Object.keys(openSlots));
-    console.log(Object.values(openSlots));
+  const changeKeys = () => {
     let tempKey = [];
     for (let i in openSlots) {
       // console.log(i);
       tempKey.push(i);
     }
     setKeySlots(tempKey);
+  };
+
+  useEffect(() => {
+    console.log(thisSkillObj);
+    console.log(openSlots);
+    console.log(Object.keys(openSlots));
+    console.log(Object.values(openSlots));
+    changeKeys();
     console.log(keySlots);
   }, []);
 
+  useEffect(() => {
+    try {
+      if (thisSkillObj["damage"]["damage_at_slot_level"]) {
+        setOpenSlots(thisSkillObj["damage"]["damage_at_slot_level"]);
+      }
+    } catch (e) {
+      console.log(e);
+    }
+  }, [thisSkillObj]);
+
+  useEffect(() => {
+    changeKeys();
+  }, [openSlots]);
+
   const SingleSlot = (props) => {
     console.log(props);
+    const handleClick = () => {
+      props.setRoll(props.roll);
+    };
     return (
-      <Button>
-        {props.slot}: {props.roll}
-      </Button>
+      <>
+        {props.slot}: <Button onClick={handleClick}>{props.roll}</Button>
+      </>
     );
   };
 
   const AllSlots = (props) => {
     return keySlots.map((i) => (
       <>
-        <SingleSlot slot={i} roll={props.openSlots[i]} />
+        <p>
+          <SingleSlot
+            setRoll={props.setRoll}
+            slot={i}
+            roll={props.openSlots[i]}
+          />
+        </p>
       </>
     ));
   };
 
   return (
-    <div className="col-md-3">
+    <div className="col-md-2 slotColumn">
+      <h4>Spell Slots</h4>
       <AllSlots
+        setRoll={props.setRoll}
         openSlots={openSlots}
         keySlots={keySlots}
-        // valueSlots={valueSlots}
       />
     </div>
   );
